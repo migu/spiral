9e0824f7687b71d5f47d6e6b55b4e48ef48c4d0f
Michael Gutbier <dev@michael-gutbier.de>
2020-03-01 16:02:04 +0100

    update project description

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject vis42 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A simple quil sketch, where shapes spiral around randomly"
+  :url "http://github.com/migu/spiral"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl-3.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [quil "3.1.0"]])
